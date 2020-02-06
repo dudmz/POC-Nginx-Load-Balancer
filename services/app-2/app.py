a65b26331b6a859e9b6d470adcac6c826c74a81a
Carlos Damázio <kaduardobsb95@gmail.com>
2020-02-06 19:35:04 -0300

    adding previous proof of concept

---
+++ services/app-2/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, escape, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return f'This is app 2!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
